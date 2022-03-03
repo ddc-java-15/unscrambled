7bb16f63dae90f8e61bffb5a84bdf7dde0386fe4
seanblann <sblann@cnm.edu>
2022-03-03 11:06:02 -0700

    Implemented ThemeViewModel.

---
+++ app/src/main/java/edu/cnm/deepdive/unscrambled/viewmodel/ThemeViewModel.java
@@ -0,0 +1,94 @@
+package edu.cnm.deepdive.unscrambled.viewmodel;
+
+import android.app.Application;
+import android.util.Log;
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.DefaultLifecycleObserver;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Transformations;
+import edu.cnm.deepdive.unscrambled.model.entity.Theme;
+import edu.cnm.deepdive.unscrambled.service.ThemeRepository;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import java.util.List;
+
+public class ThemeViewModel extends AndroidViewModel implements DefaultLifecycleObserver {
+
+  private final ThemeRepository repository;
+  private final LiveData<Theme> theme;
+  private final MutableLiveData<Throwable> throwable;
+  private final MutableLiveData<Long> themeId;
+  private final CompositeDisposable pending;
+
+
+  public ThemeViewModel(@NonNull Application application) {
+    super(application);
+    repository = new ThemeRepository(application);
+    themeId = new MutableLiveData<>();
+    theme = Transformations.switchMap(themeId, repository::get);
+    throwable = new MutableLiveData<>();
+    pending = new CompositeDisposable();
+  }
+
+  @Override
+  public void onStop(@NonNull LifecycleOwner owner) {
+    DefaultLifecycleObserver.super.onStop(owner);
+    pending.clear();
+  }
+
+  public LiveData<Theme> getTheme() {
+    return theme;
+  }
+
+  public LiveData<Throwable> getThrowable() {
+    return throwable;
+  }
+
+  public LiveData<List<Theme>> getThemes() {
+    return repository.getAll();
+  }
+
+  public void save(Theme theme) {
+    Disposable disposable = repository
+        .save(theme)
+        .subscribe(
+            (t) -> {},
+            this::postThrowable
+        );
+    pending.add(disposable);
+  }
+
+  public void delete(Theme theme) {
+    //noinspection ResultOfMethodCallIgnored
+    repository
+        .delete(theme)
+        .subscribe(
+            () -> {},
+            this::postThrowable
+        );
+  }
+
+  private void postThrowable(Throwable throwable) {
+    Log.e(getClass().getSimpleName(), throwable.getMessage(), throwable);
+    this.throwable.postValue(throwable);
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
